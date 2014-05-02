dc6e57d9002adaad12ec4d4011ecc3b957d7bf38
Wihlly <mouline.william@gmail.com>
2014-05-02 17:34:45 +0200

    Update creation.js

--- js/creation.js
+++ js/creation.js
@@ -2,80 +2,70 @@ window.onload = function() {
 	document.getElementById("ajouter1").addEventListener("click", plus, true);
 }
 
-var c,c2, ch;
- 
+var a, a1,a2, a3, nbDiv;
 // ajouter un champ avec son "name" propre;
 function plus(){
-    c=document.getElementById('form');
-    c2=c.getElementsByTagName('input');
-    ch=document.createElement('div');
+    a1=document.getElementById('allAttributs');
+	a=a1.getElementsByTagName('div');
+	nbDiv=a.length;
+    a2=a1.getElementsByTagName('input');
+    a3=document.createElement('div');
+	
+    a3.setAttribute('class','attributs');
+    a3.setAttribute('id','ajout_attribut'+nbDiv);
+    a1.appendChild(a3);
+	
+    a1=document.getElementById('ajout_attribut'+nbDiv);
+    a3=document.createElement('select');
      
-    ch.setAttribute('class','attributs');
-    ch.setAttribute('id','ajout_attribut'+c2.length/3);
-    c.appendChild(ch);
-    
-    c=document.getElementById('ajout_attribut'+c2.length/3);
-    ch=document.createElement('select');
+    a3.setAttribute('name','atribut'+nbDiv);
+    a3.setAttribute('id','atr'+nbDiv);
+    a1.appendChild(a3);
+	
+    a1=document.getElementById('atr'+nbDiv);
+    a3=document.createElement('option');
      
-    ch.setAttribute('name','atribut'+c2.length/3);
-    ch.setAttribute('id','atr'+c2.length/3);
-    c.appendChild(ch);
+    a3.setAttribute('value','Atr'+nbDiv);
+	a3.textContent="Atr"+nbDiv;
+    a1.appendChild(a3);
     
-    c=document.getElementById('atr'+c2.length/3);
-    ch=document.createElement('option');
+    a1=document.getElementById('ajout_attribut'+nbDiv);
+    a3=document.createElement('label');
      
-    ch.setAttribute('value','Atr'+c2.length/3);
-    c.appendChild(ch);
+    a3.setAttribute('for','valueattr'+nbDiv);
+	a3.textContent="Value :";
+    a1.appendChild(a3);
     
-    c=document.getElementById('ajout_attribut'+c2.length/3);
-    ch=document.createElement('label');
+    a1=document.getElementById('ajout_attribut'+nbDiv);
+    a3=document.createElement('textarea');
      
-    ch.setAttribute('for','valueattr'+c2.length/3);
-    c.appendChild(ch);
+    a3.setAttribute('name','vatr'+nbDiv);
+    a3.setAttribute('id','valueattr'+nbDiv);
+    a3.setAttribute('rows','1');
+    a1.appendChild(a3);
     
-    c=document.getElementById('ajout_attribut'+c2.length/3);
-    ch=document.createElement('textarea');
+    a1=document.getElementById('ajout_attribut'+nbDiv);
+    a3=document.createElement('input');
      
-    ch.setAttribute('name','vatr'+c2.length/3);
-    ch.setAttribute('id','valueattr'+c2.length/3);
-    ch.setAttribute('rows','1');
-    c.appendChild(ch);
+    a3.setAttribute('type','button');
+    a3.setAttribute('id','modifier'+nbDiv);
+    a3.setAttribute('name','modifier');
+    a3.setAttribute('value','Modifier');
+    a1.appendChild(a3);
     
-    c=document.getElementById('ajout_attribut'+c2.length/3);
-    ch=document.createElement('input');
+    a1=document.getElementById('ajout_attribut'+nbDiv);
+    a3=document.createElement('input');
      
-    ch.setAttribute('type','button');
-    ch.setAttribute('id','ajouter'+c2.length/3);
-    ch.setAttribute('name','ajouter');
-    ch.setAttribute('value','Ajouter');
-    c.appendChild(ch);
-    
-    c=document.getElementById('ajout_attribut'+c2.length/3);
-    ch=document.createElement('input');
-     
-    ch.setAttribute('type','button');
-    ch.setAttribute('id','modifier'+c2.length/3);
-    ch.setAttribute('name','modifier');
-    ch.setAttribute('value','Modifier');
-    c.appendChild(ch);
-    
-    c=document.getElementById('ajout_attribut'+c2.length/3);
-    ch=document.createElement('input');
-     
-    ch.setAttribute('type','button');
-    ch.setAttribute('id','supprimer'+c2.length/3);
-    ch.setAttribute('name','supprimer');
-    ch.setAttribute('value','Supprimer');
-    c.appendChild(ch);
-    
-    
-     
-    document.getElementById('ajout_attribut'+c2.length/3).style.display='inline';
+    a3.setAttribute('type','button');
+    a3.setAttribute('id','supprimer'+nbDiv);
+    a3.setAttribute('name','supprimer');
+    a3.setAttribute('value','Supprimer');
+    a1.appendChild(a3);
 }
  
 // supprimer le dernier champ;
 function moins(){
-    if(c2.length>0){c.removeChild(c2[c2.length-1])}
-    if(c2.length==0){document.getElementById('sup').style.display='none'};
+    if(a2.length>0){a1.removeChild(a2[a2.length-1])}
+    if(a2.length==0){document.getElementById('sup').style.display='none'};
 }
 
